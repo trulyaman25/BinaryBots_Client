3e7a6a111dfbb63b7779655184b710f855264d6f
Aman Kumar <trulyaman25@gmail.com>
2025-02-01 01:57:04 +0530

    Updated Responsiveness

--- src/section/memberPage.jsx
+++ src/section/memberPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { User, Mail, Linkedin, Instagram, Github, ArrowRight, MonitorSmartphone, PenTool, Code, HandshakeIcon } from 'lucide-react';
 import amanPicture from '/teamMembers/amanImg.jpg';
 import TechStackTicker from '../components/TechStackTicker';
@@ -33,9 +34,9 @@ function MemberPage() {
 	return (
 		<>
 			<div className='bg-[#eeedec]'>
-				<div className="w-full h-[102vh] rounded-b-[70px] lg:rounded-b-[120px] border-b-1 bg-[#fcfcfc] border-b-slate-300">
-					<div className='w-full h-[75vh] rounded-b-[70px] lg:rounded-b-[120px] border-b-1 border-b-slate-300 bg-[#eeedec]'>
-						<div id="nav" className='w-full h-[130px] flex flex-row justify-between items-center px-4 sm:px-10 md:px-16 lg:px-24'>
+				<div className="w-full h-[118vh] xs:h-[102vh] rounded-b-[70px] lg:rounded-b-[120px] border-b-1 bg-[#fcfcfc] border-b-slate-300">
+					<div className='w-full h-[100vh] xs:h-[75vh] flex flex-col rounded-b-[70px] lg:rounded-b-[120px] border-b-1 border-b-slate-300 bg-[#eeedec]'>
+						<div id="nav" className='w-full min-h-[100px] sm:min-h-[130px] flex flex-row justify-between items-center px-4 sm:px-10 md:px-16 lg:px-24'>
 							<div className='flex flex-row justify-between items-center gap-1 xs:gap-3'>	 
 								<div className='bg-[#eeedec] border-1 border-gray-50 p-1 rounded-full md:pl-5 flex flex-row justify-between items-center gap-4'>
 									<h1 className='text-sm text-[#131313] font-NoirProLight hidden md:block'>trulyaman25@gmail.com</h1>
@@ -69,19 +70,19 @@ function MemberPage() {
 							</div>
 						</div>
 
-						<div className='flex flex-col justify-center items-center gap-6 px-10'>
+						<div id='introSection' className='flex-grow flex flex-col justify-center items-center gap-3 sm:gap-6 px-4 sm:px-10'>
 							<img src={amanPicture} alt="Aman's Picture" className='w-[200px] h-[200px] border-5 border-white rounded-full'/>
-							<div className='flex flex-col justify-center items-center gap-3'>
+							<div className='flex flex-col justify-center items-center gap-1 sm:gap-3'>
 								<h1 className='text-3xl sm:text-5xl capitalize font-FreeZoneBold tracking-wider mt-3'>aman  kumar</h1>
 								<p className='text-2xl sm:text-4xl text-gray-500 font-FreeZoneRegular max-w-[600px] text-center'>Building digital experiences with Blockchain, AI, and Full-Stack Development.</p>
 								
-								<div className='flex flex-col sm:flex-row justify-between items-center gap-3 sm:gap-7 mt-4'>
-									<button className='bg-[#131313] text-white font-NoirProRegular text-xs px-10 py-4 rounded-full hover:cursor-pointer hover:bg-[#302f2f] hover:scale-105 transition-all duration-300 ease-in-out'>
+								<div className='flex flex-col sm:flex-row justify-between items-center gap-3 sm:border-1 border-gray-500 p-1 rounded-full sm:gap-7 mt-4 transition-none'>
+									<button className='bg-[#131313] text-white font-NoirProRegular text-xs px-10 py-4 rounded-full hover:cursor-pointer hover:bg-[#302f2f] transition-all duration-300 ease-in-out'>
 										coding profiles
 									</button>
-									<button className='bg-[#131313] text-white font-NoirProRegular text-xs px-10 py-4 rounded-full hover:cursor-pointer hover:bg-[#302f2f] hover:scale-105 transition-all duration-300 ease-in-out'>
+									<Link to='/' className='bg-[#131313] text-white font-NoirProRegular text-xs px-10 py-4 rounded-full hover:cursor-pointer hover:bg-[#302f2f] transition-all duration-300 ease-in-out'>
 										home
-									</button>
+									</Link>
 								</div>
 
 								{/* <div className='flex flex-row justify-between items-center gap-10 mt-10'>
