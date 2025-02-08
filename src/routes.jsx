789164fed3e0898ba0294e2ff289c3a6734a3114
Aman Kumar <trulyaman25@gmail.com>
2025-02-08 12:38:24 +0530

    Initialized Vercel Analytics

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from "./components/navigation/header/header";
 import Home from "./section/home";
@@ -11,6 +12,7 @@ function RoutesApp() {
 		<Router>
 			{/* <Header /> */}
 			<MainRoutes />
+			<Analytics />
 			<Footer />
 		</Router>
 	);
